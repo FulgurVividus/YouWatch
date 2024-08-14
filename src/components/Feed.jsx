b95913f0f98bd6bda1fbed14223c782f2af0dffc
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-14 19:04:14 +0500

    feat: temp Sidebar & Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,43 @@
+import { Box, Stack, Typography } from "@mui/material";
+import { Sidebar, Videos } from "./";
+
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      {/* Sidebar */}
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright ©{new Date().getFullYear()} Murodjon
+        </Typography>
+      </Box>
+
+      {/* Main Content */}
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
 }
 
 export default Feed;
