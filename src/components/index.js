6a064d848474e3a8c1492b930293be8e1c0ebedc
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-14 18:29:30 +0500

    feat: temp Sidebar without functionality

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as ChannelDetail } from "./ChannelDetail";
 export { default as SearchFeed } from "./SearchFeed";
 export { default as VideoDetail } from "./VideoDetail";
 export { default as SearchBar } from "./Searchbar";
+export { default as Sidebar } from "./Sidebar";
